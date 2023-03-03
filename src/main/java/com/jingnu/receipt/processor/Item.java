768b11d6cdb33f29be813d8b587251432144cd2e
Jingnu An <ajingnv@gmail.com>
2023-03-02 23:27:21 -0800

    add item component

--- src/main/java/com/jingnu/receipt/processor/Item.java
+++ src/main/java/com/jingnu/receipt/processor/Item.java
@@ -1,2 +1,26 @@
-package com.jingnu.receipt.processor;public class Item {
+package com.jingnu.receipt.processor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Item {
+    private String shortDescription;
+    private String price;
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
+
