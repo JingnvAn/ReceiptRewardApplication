f003177d5a2c9d661fb55ad52cf907e9daa25df3
Jingnu An <ajingnv@gmail.com>
2023-03-06 23:42:06 -0800

    Complete functionality of the API server

--- src/main/java/com/jingnu/receipt/processor/ErrorMessage.java
+++ src/main/java/com/jingnu/receipt/processor/ErrorMessage.java
@@ -1,12 +1,11 @@
 package com.jingnu.receipt.processor;
 
-import org.springframework.stereotype.Component;
 public enum ErrorMessage {
     EMPTY_PAYLOAD("Empty payload not allowed."),
     EMPTY_ITEMS_NOT_ALLOW("Empty 'items' not allowed."),
     EMPTY_INPUT_GENERAL("Invalid input provided. One or more properties missing."),
     INVALID_INPUT("Invalid input provided: %s. Property '%s' must be of '%s' pattern."),
-    RESOURCE_NOT_FOUND("The requested receipt was not found."),
+    RECEIPT_NOT_FOUND("No receipt found for id %s."),
     INTERNAL_SERVER_ERROR("An internal server error occurred."),
     JSON_EXCEPTION_MSG("Invalid request due to bad JSON format: %s");
 
--- src/main/java/com/jingnu/receipt/processor/service/ReceiptService.java
+++ src/main/java/com/jingnu/receipt/processor/service/ReceiptService.java
@@ -8,18 +8,19 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.LocalTime;
 import java.util.*;
 
 @Component
 public class ReceiptService {
     @Autowired
-    Receipt receipt;
-
-    @Autowired
     ItemService itemService;
+    private static final Logger logger = LoggerFactory.getLogger(ReceiptService.class);
 
     public Receipt createReceiptFromInput(String input) {
+        Receipt receipt = new Receipt();
         JSONObject inputJson = new JSONObject(input);
         receipt.setRetailer(inputJson.getString(ReceiptValidator.Properties.RETAILER.getValue()));
         receipt.setPurchaseDate(inputJson.getString(ReceiptValidator.Properties.PURCHASE_DATE.getValue()));
@@ -33,7 +34,6 @@ public class ReceiptService {
             JSONObject currentItem = itemsJson.getJSONObject(i);
             String shortDescription = currentItem.getString(ItemValidator.Properties.SHORT_DESCRIPTION.getValue());
             String price = currentItem.getString(ItemValidator.Properties.PRICE.getValue());
-
             items.add(itemService.createItem(shortDescription, price));
         }
 
@@ -46,6 +46,60 @@ public class ReceiptService {
     }
 
     public Integer calculatePoints(Receipt inputReceipt) {
-        return 5;
+        int points = 0;
+        //One point for every alphanumeric character in the retailer name.
+        int count1 = 0;
+        for (char c : inputReceipt.getRetailer().toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                points++;
+                count1++;
+            }
+        }
+        logger.debug(count1 + " points - retailer name has " + inputReceipt.getRetailer().length() + " characters");
+
+        //50 points if the total is a round dollar amount with no cents.
+        String roundDollarRegex = "^\\d+\\.00$";
+        if(inputReceipt.getTotal().matches(roundDollarRegex)) {
+            points += 50;
+            logger.debug(50 + " points - total is a round dollar");
+        }
+
+        //25 points if the total is a multiple of 0.25.
+        if (Double.parseDouble(inputReceipt.getTotal()) % 0.25 == 0) {
+            points += 25;
+            logger.debug(25 + " points - total is a multiple of 0.25");
+        }
+
+        // 5 points for every two items on the receipt.
+        points += Math.floor(inputReceipt.getItems().size() / 2.0) * 5;
+        logger.debug(Math.floor(inputReceipt.getItems().size() / 2.0)*5 + " points - every 2 pairs of items");
+
+        //If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
+        //The result is the number of points earned.
+        for(Item item : inputReceipt.getItems()) {
+            if (item.getShortDescription().strip().length() % 3 == 0) {
+                points += Math.ceil(Double.parseDouble(item.getPrice()) * 0.2);
+                logger.debug(Math.ceil(Double.parseDouble(item.getPrice()) * 0.2) + " points for item " + item.getShortDescription());
+            }
+        }
+
+        //6 points if the day in the purchase date is odd.
+        int dateLength = inputReceipt.getPurchaseDate().length();
+        String day = inputReceipt.getPurchaseDate().substring(dateLength-2, dateLength); //sub string the last two char to find the day
+        if (Integer.parseInt(day) % 2 != 0) {
+            points += 6;
+            logger.debug("6  points - day in the purchase date is odd");
+        }
+
+        //10 points if the time of purchase is after 2:00pm and before 4:00pm.
+        LocalTime purchaseTime = LocalTime.parse(inputReceipt.getPurchaseTime());
+        LocalTime start = LocalTime.of(14, 0); // 2:00pm
+        LocalTime end = LocalTime.of(16, 0); // 4:00pm
+        if (!purchaseTime.isBefore(start) && purchaseTime.isBefore(end)) {
+           points += 10;
+           logger.debug("10 points - the time of purchase is after 2:00pm and before 4:00pm");
+        }
+
+        return points;
     }
 }
